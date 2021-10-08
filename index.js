11c09143c89bd37038d9da3dd8ddfce7cea62819
Evelyn <evelyn.chavez92@yahoo.com>
2021-10-08 09:21:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-let cats = ['Milo', 'Otis', 'Garfield']
-cats.length=0;
-cats.push('Milo','Otis', 'Garfield');
+const cats = ['Milo', 'Otis', 'Garfield']
 
- let destructivelyAppendCat = cats.push('Ralph');
+
+ let destructivelyAppendCat = function (Ralph){
+   cats.push('Ralph')
+ }
+
+ let destructivelyPrependCat = function (Bob){
+cats.unshift(Bob)
+ }
+
+ let destructivelyRemoveLastCat = function (){
+     cats.pop()
+ }
+
+ let destructivelyRemoveFirstCat = function (){
+     cats.shift()
+ }
+
+ let appendCat = function(Broom){
+  return [...cats,Broom]
+ }
+
+ let prependCat = function (Arnold){
+     return [Arnold, ...cats]
+ }
+
+ let removeLastCat = function (){
+     return cats.slice(0, -1)
+ }
+
+ let removeFirstCat = function(){
+     return cats.slice(1)
+ }
